bba779faf66352d06af1b3986c1ef312f82de20f
sdamke90 <34995841+sdamke90@users.noreply.github.com>
2018-04-03 01:12:49 +0530

    Added CSS file for courses

---
+++ courses.js
@@ -0,0 +1,62 @@
+function submitDetails(){
+    var fName = document.forms["applicantDetails"]["firstName"].value;
+    var lName = document.forms["applicantDetails"]["lastName"].value;
+    var contact = document.forms["applicantDetails"]["mobile"].value;
+    var eID = document.forms["applicantDetails"]["emailId"].value;
+    
+    if(fName  == "")
+     {
+         alert("Please enter first mame");
+         return;
+     }
+     else if(lName  == "")
+        {
+            alert("Please enter last name");
+            return;
+        }
+    else if(contact ==  "")
+        {
+            alert("Please enter contact number");
+            return;
+        }
+    else if(eID == "")
+        {
+           alert("Plese enter email address");
+           return;
+        }
+    else
+      {
+          submitajax(fName,lName,contact,eID);
+          return;
+      }   
+}
+
+
+function submitajax(fName,lName,contact,eID){
+    console.log("submit ajax initiated"); 
+    
+    var submitter;
+     
+     var valueString =  "";
+      
+     valueString = "fname=" + fName + "&lname=" + lName + "&contact=" +  contact + "&email=" + eID ; 
+     
+    console.log(valueString);  
+
+     submitter = new XMLHttpRequest();
+
+     submitter.open("POST","apply.php", true);
+     submitter.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+     submitter.send(valueString);
+
+    submitter.onreadystatechange = function() {
+        if(submitter.readyState == 4 && submitter.status == 200) {
+            console.log("Data Saved successfully");
+        }
+    }
+
+    console.log(submitter);
+
+
+}
+
